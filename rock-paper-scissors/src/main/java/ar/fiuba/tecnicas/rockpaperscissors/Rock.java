0a678c7a2961033fcb639a97381d3a3a34c69ec5
LucasRob <lucas.jose.robles@gmail.com>
2018-10-07 16:45:59 -0300

    Tp0

---
+++ rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Rock.java
@@ -0,0 +1,17 @@
+package ar.fiuba.tecnicas.rockpaperscissors;
+
+public class Rock {
+
+	public Rock vs(Scissors scissors) {
+		return this;
+	}
+
+	public Paper vs(Paper paper) {
+		return paper;
+	}
+
+	public Rock vs(Rock rock) {
+		return this;
+	}
+
+}
